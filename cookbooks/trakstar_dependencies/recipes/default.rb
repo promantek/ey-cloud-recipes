c30e04aa75d8f8b4311755223bf6ee2cc5fb901c
Ken Everett <ken.everett@gmail.com>
2011-07-20 13:18:38 -0700

    dependencies installed.  should pre-package the linux source to speed things up.

--- cookbooks/trakstar_dependencies/recipes/default.rb
+++ cookbooks/trakstar_dependencies/recipes/default.rb
@@ -12,6 +12,7 @@ end
 execute "sudo ln -sf /usr/share/zoneinfo/US/Pacific /usr/share/zoneinfo/localtime" do
 end
 
+
 directory '/data/dist/' do
   owner 'deploy'
   group 'users'
@@ -19,7 +20,23 @@ directory '/data/dist/' do
 end
 
 # install MSSQL adapter dependencies for r6 import
-execute install_unixODBC do
+build_unix_src = "./configure ;
+                  make ;
+                  sudo make install"
+
+install_unix_odbc =
+    "wget ftp://ftp.unixodbc.org/pub/unixODBC/unixODBC-2.3.0.tar.gz ;
+     tar -xzvf unixODBC-2.3.0.tar.gz ;
+     cd unixODBC-2.3.0 ;
+     #{build_unix_src}"
+
+install_freetds =
+   "wget http://ibiblio.org/pub/Linux/ALPHA/freetds/stable/freetds-stable.tgz ; 
+   tar -xzvf freetds-stable.tgz ;
+   cd freetds-0.82 ;
+   #{build_unix_src}"
+
+execute install_unix_odbc do
   cwd "/data/dist"
 end
 
@@ -27,32 +44,12 @@ execute install_freetds do
   cwd "/data/dist"
 end
 
-# remove selenium, causes production dependency issues
-execute "cd /data/trakstar/current ; rm -rf #{troublesome_files_causing_class_loading_issues} " do
-  cwd "/data/dist"
-end
+## remove selenium, causes production dependency issues
+#execute "cd /data/trakstar/current ; rm -rf #{troublesome_files_causing_class_loading_issues} " do
+#  cwd "/data/dist"
+#end
 
-
-def install_unixODBC
-  "wget ftp://ftp.unixodbc.org/pub/unixODBC/unixODBC-2.3.0.tar.gz ;
-   tar -xzvf unixODBC-2.3.0.tar.gz ;
-   cd unixODBC-2.3.0 ;
-   #{configure_make_install}"
-end
-
-def install_freetds
-  "wget http://ibiblio.org/pub/Linux/ALPHA/freetds/stable/freetds-stable.tgz ;
-   tar -xzvf freetds-stable.tgz ;
-   cd freetds-0.82 ;
-   #{configure_make_install}"
-end
-
-def configure_make_install
-  "./configure ;
-   make ;
-   sudo make install"
-end
-
-def troublesome_files_causing_class_loading_issues
-  "spec/selenium spec/support/selenium_spec_helper.rb lib/tasks/rspec_mods.rake lib/tasks/rspec_plugins.rake rm lib/tasks/initech.rake"
-end
+#
+#def troublesome_files_causing_class_loading_issues
+#  "spec/selenium spec/support/selenium_spec_helper.rb lib/tasks/rspec_mods.rake lib/tasks/rspec_plugins.rake rm lib/tasks/initech.rake"
+#end
