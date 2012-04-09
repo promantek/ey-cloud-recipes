5f2a66e34d7448ae94fe243a0b67ce7acf7e0ca2
Ken Everett <ken.everett@gmail.com>
2012-04-09 13:48:59 -0700

    cronjobs for app master on the EY dashboard

--- cookbooks/cron_check/recipes/default.rb
+++ cookbooks/cron_check/recipes/default.rb
@@ -1,27 +1,29 @@
 # Recipe for testing cron is working properly.
 #
-cron "timestamp_check" do
-  user node[:owner_name]
-  minute "*"
-  hour "*"
-  day "*"
-  month "*"
-  weekday "*"
-  command "touch /home/#{node[:owner_name]}/cron.timestamp.check"
-  action :create
-end
 
-template "/etc/monit.d/cron_timestamps.monitrc" do
-  backup 0
-  source "cron_check.monitrc.erb"
-  owner "root"
-  group "root"
-  mode 0655
-  variables({
-  :owner => node[:owner_name]
-  })
-end
 
-execute "reload monit" do
-  command "monit reload"
-end
+if node[:instance_role] == 'db_master' 
+  cron "task_name" do 
+    #minute '*/15' 
+    user 'deploy' 
+    command "/usr/bin/scout d6c45d0e-ff1f-4851-944f-2a4f5e64bc3d"
+  end 
+end 
+
+if node[:name] == 'redis' 
+  cron "task_name" do 
+    #minute '*/15' 
+    user 'deploy' 
+    command "/usr/bin/scout 856c1874-e793-43b6-8098-f1e4fb320ec5"
+  end 
+end 
+#
+# redundant with the EY use cronjob
+#
+#if node[:instance_role] == 'app_master'
+  #cron "task_name" do 
+    ##minute '*/15' 
+    #user 'deploy' 
+    #command "/usr/bin/scout 91fe7a32-64f3-4536-898b-b76ccee9e330"
+  #end 
+#end 
