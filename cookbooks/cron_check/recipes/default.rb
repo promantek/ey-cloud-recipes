931e5365c3c76790f54a6b96d62d8bc21ec225c7
Ken Everett <ken.everett@gmail.com>
2012-10-09 14:35:36 -0700

    cron db and s3 periodic backups

--- cookbooks/cron_check/recipes/default.rb
+++ cookbooks/cron_check/recipes/default.rb
@@ -9,9 +9,22 @@ if node[:instance_role] == 'db_master'
   cron "task_name" do 
     #minute '*/15' 
     user 'deploy' 
-    command "/usr/bin/scout d6c45d0e-ff1f-4851-944f-2a4f5e64bc3d"
+     command "/usr/bin/scout d6c45d0e-ff1f-4851-944f-2a4f5e64bc3d"
   end 
-end 
+
+  cron "db backups" do
+    hour "2"
+    minute "0"
+    command "/mnt/backups/backup_postgres.sh"
+  end
+
+  cron "s3 backups" do
+    hour "2"
+    minute "0"
+    command "/mnt/backups/backup_s3_assets.sh"
+  end
+
+end
 
 if node[:name] == 'app_master' 
   cron "task_name" do 
